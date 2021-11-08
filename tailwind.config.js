7c9cc56d7ddeefeac48542493088270d3be399c3
YarnNoir <billyoneid@outlook.co.id>
2021-11-08 21:06:09 +0700

    stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,37 @@
-const {
-  guessProductionMode
-} = require("@ngneat/tailwind");
+const { guessProductionMode } = require("@ngneat/tailwind");
 
-process.env.TAILWIND_MODE = guessProductionMode() ? 'build': 'watch';
+process.env.TAILWIND_MODE = guessProductionMode() ? "build" : "watch";
 
 module.exports = {
-  prefix: '',
-  mode: 'jit',
+  prefix: "",
+  mode: "jit",
   purge: {
-    content: [
-      './src/**/*.{html,ts,css,scss,sass,less,styl}',
-    ]
+    content: ["./src/**/*.{html,ts,css,scss,sass,less,styl}"],
   },
-  darkMode: 'class',
+  darkMode: "class",
   // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        'trans-white': 'rgba(255, 255, 255, 0.2)',
-        'trans-black': 'rgba(10, 10, 10, 0.2)',
+        "trans-white": "rgba(255, 255, 255, 0.2)",
+        "trans-black": "rgba(10, 10, 10, 0.2)",
+        "ripple-white": `rgba(255, 255, 255, 0.1)`,
+        "ripple-black": `rgba(0, 0, 0, 0.1)`,
       },
       boxShadow: {
-        'dark': '0 4px 6px -1px rgba(255, 255, 255, 0.3), 0 2px 4px -1px rgba(255, 255, 255, 0.4)'
+        dark: "0 4px 6px -1px rgba(255, 255, 255, 0.3), 0 2px 4px -1px rgba(255, 255, 255, 0.4)",
       },
       dropShadow: {
-        'dark': '0 4px 3px rgba(255, 255, 255, 0.4)'
-      }
+        dark: "0 4px 3px rgba(255, 255, 255, 0.4)",
+      },
     },
   },
   variants: {
-    extend: {
-      
-    },
+    extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
 };
